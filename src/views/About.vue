d4f0ff1258d612da9eb5063906e6b6292fe0f7d2
姜志豪20308 <zhihao.jiang@hand-china.com>
2019-09-02 16:23:26 +0800

    add: vant和移动端自适应配置

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Button type="default">默认按钮</Button>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { Button } from 'vant';
+
+@Component({
+  components: {
+    Button,
+  },
+})
+export default class About extends Vue { }
+</script>
+
+<style lang="scss" scoped>
+</style>
