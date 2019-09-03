3453b237473f6927917535ff90104c551a96f069
姜志豪20308 <zhihao.jiang@hand-china.com>
2019-09-03 16:00:15 +0800

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,17 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
+  <!-- <NavBar title="标题" left-text="返回" right-text="按钮" left-arrow></NavBar> -->
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { NavBar } from 'vant';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
   components: {
+    NavBar,
     HelloWorld,
   },
 })
